65bf728a11c2f447263cb66552e63cd46bd5b77e
Renal Eki Riyanto <renalekiriyanto@gmail.com>
2022-09-25 20:41:59 +0700

    delete, update, get by id activity

--- backend/routes/api/activities.js
+++ backend/routes/api/activities.js
@@ -4,6 +4,19 @@ const router = express.Router();
 
 const Activities = require('../../models/Activities');
 
+//@route    GET /api/activities
+//@desc     Get activities
+//@access   Public
+router.get('/', async(req, res) => {
+   try{
+       const activities = await Activities.find();
+       res.json(activities);
+   } catch (err){
+       console.error(err.message);
+       res.status(500).send('Server error');
+   }
+});
+
 //@route    POST /api/activities
 //@desc     Add activities
 //@access   Public
@@ -32,5 +45,60 @@ router.post('/', [
        res.status(500).send('Server error');
    }
 });
+
+//@route    GET /api/activities/:id
+//@desc     Get activity by Id
+//@access   Public
+router.get('/:id', async(req, res) => {
+   try {
+       const activity = await Activities.findById(req.params.id);
+       res.json(activity);
+   } catch (err){
+       console.error(err.message);
+       res.status(500).send('Server error');
+   }
+});
+
+//@route    PUT /api/activities/:id
+//@desc     Edit activity
+//@access   Public
+router.put('/:id', [
+    check('name', 'Name is required').notEmpty(),
+    check('bell', 'Bell is required').notEmpty(),
+],async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    try {
+        const newValue = {
+            name: req.body.name,
+            bell: req.body.bell
+        };
+        const activity = await Activities.findByIdAndUpdate(req.params.id, newValue);
+        res.json({data: activity, msg: 'Updated successfully'});
+    }catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server error');
+    }
+});
+
+//@route    DELETE /api/activities/:id
+//@desc     Edit activity
+//@access   Public
+router.delete('/:id', async(req, res)=>{
+   try {
+       const activity = await Activities.findByIdAndRemove(req.params.id);
+       res.json({
+           data: activity,
+           msg: 'Delete data successfully'
+       });
+   } catch(err){
+       console.error(err.message);
+       res.status(500).send('Server error');
+   }
+});
+
 
 module.exports = router;
